bc66a648d550d6a44d18c1870aabd68e0ced8280
robinonsay <raomax@gmail.com>
2016-01-30 14:09:55 -0500

    Added sensors

--- src/org/usfirst/frc/team245/robot/Sensors.java
+++ src/org/usfirst/frc/team245/robot/Sensors.java
@@ -1,5 +1,27 @@
 package org.usfirst.frc.team245.robot;
 
-public class Sensors {
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 
+public class Sensors {
+	//Digital
+	private static Encoder driveEncoder;
+	private static Encoder winchEncoder;
+	private static DigitalInput intakeArmPhotoEye;
+	//Analog
+	private static AnalogPotentiometer armPot;
+	private static AnalogGyro robotGyro;
+	
+	public static void init(){
+		//Digital
+		driveEncoder = new  Encoder(0,0);
+		winchEncoder = new Encoder(0,0);
+		intakeArmPhotoEye = new DigitalInput(0);
+		
+		//Analog
+		armPot = new AnalogPotentiometer(0);
+		robotGyro = new AnalogGyro(0);
+	}
 }
