5edf4eb085a97033b498a24671728dbe2426a42f
robinonsay <raomax@gmail.com>
2016-02-01 20:41:07 -0500

    Added Camera test code

--- src/org/usfirst/frc/team245/robot/Robot.java
+++ src/org/usfirst/frc/team245/robot/Robot.java
@@ -3,6 +3,8 @@ package org.usfirst.frc.team245.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
 import com.github.adambots.stronghold2016.arm.Arm;
+import com.github.adambots.stronghold2016.camera.Target;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -36,6 +38,7 @@ public class Robot extends IterativeRobot {
 
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		Target.init();
 	}
 
 	/**
@@ -49,6 +52,9 @@ public class Robot extends IterativeRobot {
 
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+		for(double area: Target.getArea()){
+			System.out.println("The area is "+area);
+		}
 	}
 
 	/**
@@ -99,14 +105,15 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 
-		if (Gamepad.secondary.getA()) {
-			Arm.rollers(-1);
-		}
-		if (Gamepad.secondary.getB()) {
-			Arm.rollers(1);
-		}
-
-		Arm.winch(Gamepad.secondary.getRightY());
+//		if (Gamepad.secondary.getA()) {
+//			Arm.rollers(-1);
+//		}
+//		if (Gamepad.secondary.getB()) {
+//			Arm.rollers(1);
+//		}
+//
+//		Arm.winch(Gamepad.secondary.getRightY());
+		
 	}
 
 	/**
