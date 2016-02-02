7b645756aadfee2dea9a42ed58197923fe6b4dec
Scott Guest <Scott.Guest@live.com>
2016-02-01 19:45:07 -0500

    winch motor
    ADDED WINCH MOTORS LOGIC

--- src/com/github/adambots/stronghold2016/arm/Arm.java
+++ src/com/github/adambots/stronghold2016/arm/Arm.java
@@ -3,8 +3,13 @@ package com.github.adambots.stronghold2016.arm;
 import org.usfirst.frc.team245.robot.Actuators;
 
 public class Arm {
-	
-	public static void rollers(double speed){
+
+	public static void rollers(double speed) {
 		Actuators.getBoulderIntakeMotor().set(speed);
+	}
+
+	public static void winch(double speed) {
+		Actuators.getArmWinchMotor1().set(speed);
+		Actuators.getArmWinchMotor1().set(-speed);
 	}
 }
