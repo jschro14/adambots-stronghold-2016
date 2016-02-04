ddd7c416a19869b147232b81bf353bad87536d0d
Scott Guest <Scott.Guest@live.com>
2016-02-04 18:59:22 -0500

    winch motor
    added winch motor pneumatic release

--- src/com/github/adambots/stronghold2016/arm/Arm.java
+++ src/com/github/adambots/stronghold2016/arm/Arm.java
@@ -1,15 +1,28 @@
 package com.github.adambots.stronghold2016.arm;
 
 import org.usfirst.frc.team245.robot.Actuators;
+import org.usfirst.frc.team245.robot.Gamepad;
+
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Arm {
+	static boolean fired = false;
 
 	public static void rollers(double speed) {
 		Actuators.getBoulderIntakeMotor().set(speed);
 	}
 
 	public static void winch(double speed) {
+		if (speed < 0 && fired == true) {
+			Actuators.getWinchRatchetPneumatic().set(false);
+			fired = false;
+		} else if(speed > 0 && fired == false){
+			Actuators.getWinchRatchetPneumatic().set(true);
+			fired = true;
+		}
 		Actuators.getArmWinchMotor1().set(speed);
 		Actuators.getArmWinchMotor1().set(-speed);
+
 	}
+
 }
