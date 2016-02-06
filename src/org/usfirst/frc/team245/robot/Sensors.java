c0d5e691d68b9b0d09f1d8aaaf667bb6462bff7f
Leogomez <leogomez606@yahoo.com>
2016-02-06 13:35:01 -0500

    renamed drive encoders
    now its driveEncoderLeft and driveEncoderRight

--- src/org/usfirst/frc/team245/robot/Sensors.java
+++ src/org/usfirst/frc/team245/robot/Sensors.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj.Encoder;
 
 public class Sensors {
 	// Digital
-	private static Encoder driveEncoder1;
-	private static Encoder driveEncoder2;
+	private static Encoder driveEncoderLeft;
+	private static Encoder driveEncoderRight;
 	private static Encoder winchEncoder;
 	private static DigitalInput intakeArmPhotoEye;
 	private static DigitalInput boulderCanLaunchPhotoEye;
@@ -23,8 +23,8 @@ public class Sensors {
 
 	public static void init() {
 		// Digital
-		driveEncoder1 = new Encoder(0, 0);
-		driveEncoder2 = new Encoder(0, 0);
+		driveEncoderLeft = new Encoder(0, 0);
+		driveEncoderRight = new Encoder(0, 0);
 		winchEncoder = new Encoder(0, 0);
 		intakeArmPhotoEye = new DigitalInput(0);
 		boulderCanLaunchPhotoEye = new DigitalInput(0);
@@ -50,12 +50,17 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the driveEncoder
+	 * @return the driveEncoderLeft
 	 */
-	public static Encoder getDriveEncoder() {
-		return driveEncoder;
+	public static Encoder getDriveEncoderLeft() {
+		return driveEncoderLeft;
 	}
-
+	/**
+	 * @return the driveEncoderRight
+	 */
+	public static Encoder getDriveEncoderRight() {
+		return driveEncoderRight;
+	}
 	/**
 	 * @return the winchEncoder
 	 */
